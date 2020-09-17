161622d493bb54c9b9f24ac00238fdfb4a7846bf
jaeden Haste-pollard <jaedenh@wainuiomatahigh.school.nz>
2020-09-17 23:36:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import math 
+"""
+what is this program going to do 
+
+1.	WAIKATO AIR is based in Hamilton and flies to 3 regular destinations, Auckland, Wellington and Rotorua.
+
+2.	Each day they send an email to their customers with details of their discount fares (‘discount fare’, ‘percentage discount’ and ‘discount type’) for people who can fly the next day. The details of the discounts vary each day depending on how many seats are left on each flight.
+
+3.	WAIKATO AIR needs a program that will produce text detailing these discounts that WAIKATO AIR can then copy into a bulk email that they will send to all their members (you are only required to produce the text for WAIKATO AIR to copy. You do not produce the actual email).
+
+"""
