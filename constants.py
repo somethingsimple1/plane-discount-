28eaedf3b7f749f8ea745bd702340df9d095edc1
jaeden Haste-pollard <jaedenh@wainuiomatahigh.school.nz>
2020-11-10 08:40:34 +0000

    made the dictionary for all the different flights

---
+++ constants.py
@@ -0,0 +1,51 @@
+
+destination = {
+  'auckland to Wellington' : {
+    'max_nub_of_seats' : 500,
+    'nub_of_sets_left' : 345,
+    'price' : 233
+
+
+    },
+
+  'auckland to Rotorua' : {
+    'max_nub_of_seats' : 500,
+    'nub_of_sets_left' : 36,
+    'price' : 467
+
+  },
+
+  'Wellington to Auckland' : {
+    'max_nub_of_seats' : 500,
+    'nub_of_sets_left' : 104,
+    'price' : 405
+
+
+  },
+
+  'Wellington to Rotorua' : {
+    'max_nub_of_seats' : 500,
+    'nub_of_sets_left' : 89,
+    'price' : 451
+
+  },
+  
+  'Rotorua to Auckland' : {
+    'max_nub_of_seats' : 500,
+    'nub_of_sets_left' : 201,
+    'price' : 330
+  },
+
+  'Rotorua to Wellington' : {
+    'max_nub_of_seats' : 500,
+    'nub_of_sets_left' : 186,
+    'price' : 415
+  }
+}
+
+
+
+
+
+
+
