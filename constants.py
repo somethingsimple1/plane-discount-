4b9bd1c95651c18a5637e2cb5358e08bd08af1e2
jaeden Haste-pollard <jaedenh@wainuiomatahigh.school.nz>
2020-11-25 09:37:36 +0000

    last comment in the constants file

--- constants.py
+++ constants.py
@@ -1,8 +1,20 @@
 
 
+"""This is where the dictiony for all the flights are the dictiony is called flight_nub and in it is all the flight details like max seats, number of seats left the price of the flight, and where its going to and from 
+way it works 
 
+'1' is the flight number Note: this always has to be an integer 
+1 : {
+    'max_nub_of_seats' : 500,    #this is where the max seats on the plane are   (Note: this always has to be an integer)
+    'nub_of_seats_left' : 345,   #this is here the number of seats left on the   (Note: this always has to be an integer)
+    'price' : 233,               # this is where the price of the flight is      (Note: this always has to be an integer)
+    'destination' : 'auckland to Wellington'  #this is where the destination of the flight is 
+  }
+
+"""
 
 flight_nub = {
+  
   1 : {
     'max_nub_of_seats' : 500,
     'nub_of_seats_left' : 345,
@@ -54,4 +66,5 @@ flight_nub = {
   }
 }
 
+#dictiony for all the users will be stored here 
 user = {}
